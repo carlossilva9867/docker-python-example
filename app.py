c41028eb787c689fb5bad8fa79f85a741bc882c9
Carlos Silva <carlos.silva9867@gmail.com>
2020-10-20 21:32:14 -0300

    AC 4 Carlos Silva

--- app.py
+++ app.py
@@ -19,3 +19,7 @@ def nao_entre_em_panico():
         resposta+= str(proximo) + ","
 
     return resposta 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
